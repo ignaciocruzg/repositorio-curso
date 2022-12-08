dd9969432d641058cf032bb491f547e3e77bae62
Ignacio Alonso de la Cruz <ignacio.cruzg@bancoazteca.com>
2022-12-08 17:38:08 -0600

    Se agrega estructura principal del proyecto

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/hola', (req, res) => {
+  res.send('ruta Hola');
+});
+
+app.get('/adios', (req, res) => {
+    res.send('ruta Adiós');
+  });
+
+app.post('/registro', (req, res) => {
+    res.send('recibiendo información de registo');
+});
+
+app.post('/login', (req, res) =>{
+    res.send('ruta de login nueva');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
